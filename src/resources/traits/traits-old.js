67c915207255516ae9cd4f8af5ad7380ea7e8e5f
SpaceWarf <space_warf@live.ca>
2019-10-22 21:50:48 -0400

    Add full data set for skills, traits and other race lifepaths

---
+++ src/resources/traits/traits-old.js
@@ -0,0 +1,633 @@
+export default
+    [
+        {
+            name: 'Abashed',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Abnormally Long Tongue',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Abused',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Aches and Pains',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Addicted',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Adorable',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Affinity for...',
+            type: 'Dt',
+            cost: 4,
+            description: 'description'
+        },
+        {
+            name: 'Affinity for Business',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Affinity for Horses',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Aggressive',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Agoraphobic',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Agreeable',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Alarming',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Alcoholic',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Alert',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Alms-Taker',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Aloof',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Ambidextrous',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Ambitious',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Amenable',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Amenable to Other Options',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Anemic',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Apostate',
+            type: 'Dt',
+            cost: 3,
+            description: 'description'
+        },
+        {
+            name: 'Appreciation for Beauty',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Apprehensive',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Arcane',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Arrogant',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Artful Dodger',
+            type: 'Dt',
+            cost: 5,
+            description: 'description'
+        },
+        {
+            name: 'Atravieso',
+            type: 'Dt',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Innocence',
+            type: 'C-O',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Determination',
+            type: 'C-O',
+            cost: 5,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Fear',
+            type: 'Dt',
+            cost: 5,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Holiness',
+            type: 'Dt',
+            cost: 7,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Malevolence',
+            type: 'Dt',
+            cost: 6,
+            description: 'description'
+        },
+        {
+            name: 'Aura of Martyrdom',
+            type: 'Dt',
+            cost: 3,
+            description: 'description'
+        },
+        {
+            name: 'Authoritative',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Back-Breaking Labor',
+            type: 'C-O'
+        },
+        {
+            name: 'Bad Egg',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Barker',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Base Humility',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bastard',
+            type: 'Dt',
+            cost: 4,
+            description: 'description'
+        },
+        {
+            name: 'Batshit',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bedside Manner',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Beespeaker',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Believer',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Bellicose',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Big Boned',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bilge-Drinker',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Birdie Talk',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'A Bit Deaf',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Bitter',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Blank Stare',
+            type: 'Dt',
+            cost: 3,
+            description: 'description'
+        },
+        {
+            name: 'Blasphemer',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Blind',
+            type: 'Dt',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Blisters',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bloviatic',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Boaster',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bookworm',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Booming Voice',
+            type: 'C-O',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Bored',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Born to Be King',
+            type: 'Char',
+            cost: 5,
+            description: 'description'
+        },
+        {
+            name: 'Bottomless Stomach',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Brash',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Brave',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Broken',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Broken In',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Broken Man',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bruiser',
+            type: 'C-O',
+            description: 'description'
+        },
+        {
+            name: 'Brusque',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Brutal',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bulbous Nose',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Bull\'s-eye Spitter',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Burial Rites',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Burial at Sea',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Buxom',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cadaverous',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Call of the Sea',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Callous',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Calloused',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Callow',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Catamite',
+            type: 'Char',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Charismatic',
+            type: 'C-O',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Charming',
+            type: 'C-O',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Child Prodigy',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Chosen One',
+            type: 'Dt',
+            cost: 5
+        },
+        {
+            name: 'Chronologue',
+            type: 'Char',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Chuffing',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Chuntering',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cipher',
+            type: 'Dt',
+            cost: 5,
+            description: 'description'
+        },
+        {
+            name: 'Circumspect',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Claustrophobic',
+            type: 'Dt',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Clumsy',
+            type: 'Dt',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Cold Blooded',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Cold Hearted',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Collector',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Color Blind',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Colorful',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Comely',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Comfortable Shoes',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Commanding Aura',
+            type: 'Dt',
+            cost: 6
+        },
+        {
+            name: 'Complaining',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Compulsive Liar',
+            type: 'Char',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Condescending',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Connected',
+            type: 'Dt',
+            cost: 4
+        },
+        {
+            name: 'Confident',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Contortionist',
+            type: 'C-O',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Cookie',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cool Headed',
+            type: 'Dt',
+            cost: 2,
+            description: 'description'
+        },
+        {
+            name: 'Corrupt',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cowardly',
+            type: 'Dt',
+            cost: 1,
+            description: 'description'
+        },
+        {
+            name: 'Cramped Hands',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Crippled',
+            type: 'Dt',
+            description: 'description'
+        },
+        {
+            name: 'Crochety',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cruel',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cry Wolf',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cryptic',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Curious',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Curses like a Sailor',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cursing',
+            type: 'Char',
+            cost: 1
+        },
+        {
+            name: 'Cynical',
+            type: 'Char',
+            cost: 1
+        }
+    ]
